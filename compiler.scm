fb5d7b82fd1b20a4c7d0a4a57b049a6236873b09
unknown <amitzoh@post.bgu.ac.il>
2016-12-02 20:07:16 +0200

    Finished

--- compiler.scm
+++ compiler.scm
@@ -544,7 +544,6 @@
                (lambda(x) (list 'const x)))))
 
 
-(define _nil (const-pattern-rule null?))
 (define _boolean (const-pattern-rule boolean?))
 (define _character (const-pattern-rule char?))
 (define _number (const-pattern-rule number?))
@@ -554,12 +553,11 @@
 
 (define _quote
          (pattern-rule
-              (lambda(exp) (and (list? exp) (equal? (car exp) 'quote)))
-              (lambda(x) (list 'const (cadr exp)))))
+              `(quote ,(? 'exp))
+              (lambda (exp) `(const ,exp))))
 
 (define _const
      (compose-patterns
-          _nil
           _vector
           _boolean
           _character
@@ -590,11 +588,21 @@
                (lambda (test then)
                `(if3 ,(parse test) ,(parse then) ,(parse (void)))))))
 
-(define _disjunction
+(define _or
      (pattern-rule
           `(or . ,(? 'bodies))
                (lambda (bodies)
-               (cons 'or (map parse bodies)))))
+               (cond ((null? bodies) (parse '#f))
+                     ((eq? (length bodies) 1) (parse (car bodies)))
+                     (else `(or ,(map parse bodies)))))))
+
+(define _and
+     (pattern-rule
+          `(and . ,(? 'bodies))
+               (lambda (bodies)
+               (cond ((null? bodies) (parse '#t))
+                     ((eq? (length bodies) 1) (parse (car bodies)))
+                     (else (parse `(if ,(car bodies) (and ,@(cdr bodies)) #f)))))))
 
 (define improper-list-last
         (lambda(l)
@@ -611,10 +619,11 @@
           `(lambda ,(? 'params) . ,(? 'bodies))
                (lambda (params bodies)
                          (let ((bodies (map parse bodies))
-                              (new-params (improper-list-remove-last params)))
-                             (cond ((list? params) `(lambda-simple ,params ,@bodies))
-                                   ((pair? params) `(lambda-opt ,new-params ,(improper-list-last params) ,@bodies))
-                                   (else `(lambda-var ,params ,@bodies)))))))
+                              (new-params (improper-list-remove-last params))
+                              (seq (parse `(begin ,@bodies))))
+                                (cond ((list? params) `(lambda-simple ,params ,seq))
+                                   ((pair? params) `(lambda-opt ,new-params ,(improper-list-last params) ,seq))
+                                   (else `(lambda-var ,params ,seq)))))))
 
 (define _define
       (pattern-rule
@@ -622,26 +631,196 @@
                (lambda (var def)
                   (if (pair? var)
                   (let ((exp (parse `(lambda ,(cdr var) ,def))))
-                    `(define ,(parse (car var)) ,exp))
-                  `(define ,(parse var) ,(parse def))))))
+                    `(def ,(parse (car var)) ,exp))
+                    `(def ,(parse var) ,(parse def))))))
 
 (define _application
       (pattern-rule
           `(,(? 'func) . ,(? 'params))
                (lambda (func params)
-                  (let ((func (parse func))
+                  (if (null? func)
+                     (error 'done (format "Unknown form: ~s" func))
+                     (let ((func (parse func))
                         (params (map parse params)))
-                        `(applic ,func ,params)))))
+                        `(applic ,func ,params))))))
+
+(define _cond
+      (pattern-rule
+          `(cond . ,(? 'bodies))
+               (lambda (bodies)
+                    (cond ((null? bodies) (parse (void)))
+                          ((eq? (caar bodies) 'else)
+                              (parse `(begin ,@(cdar bodies))))
+                     (else
+                             (parse `(if ,(caar bodies) (begin ,@(cdar bodies)) (cond ,@(cdr bodies)))))))))
+
+
+(define _begin
+      (pattern-rule
+          `(begin . ,(? 'bodies))
+               (lambda (bodies)
+                  (let ((bodies (map parse bodies)))
+                    (cond ((null? bodies) (parse (void)))
+                         ((eq? (length bodies) 1) (car bodies))
+                         (else `(seq ,bodies)))))))
+
+(define _let
+      (pattern-rule
+          `(let ,(? 'params) . ,(? 'bodies))
+               (lambda (params bodies)
+                      (parse `((lambda ,(map car params) ,@bodies)  ,@(map cadr params))))))
+
+(define _let*
+      (pattern-rule
+          `(let* ,(? 'params) . ,(? 'bodies))
+               (lambda (params bodies)
+                      (cond ((null? params)(parse `(let ,params ,@bodies)))
+                            ((eq? (length params) 1) (parse `(let ,params ,@bodies)))
+                            (else (parse `(let ,(list (car params)) (let* ,(cdr params) ,@bodies))))))))
+
+(define _letrec
+      (pattern-rule
+          `(letrec ,(? 'params) . ,(? 'bodies))
+               (lambda (params bodies)
+                    (let ((set-bodies (map (lambda (x y) `(set! ,x ,y))
+                                       (map car params) (map cadr params)))
+                          (false-params (map (lambda (x y) `(,x #f))
+                                           (map car params) (map cadr params)))
+                          (let-bodies `((let () ,@bodies))))
+                         (parse `(let ,false-params ,@(append set-bodies let-bodies)))))))
+
+(define _set!
+     (pattern-rule
+          `(set! ,(? 'arg) ,(? 'val))
+             (lambda (arg val)
+                 `(set ,(parse arg) ,(parse val)))))
+
+(define _quasiquote
+     (pattern-rule
+             (lambda (exp) (if (eq? (car exp) 'quasiquote) (cdr exp) #f))
+             (lambda (exp)
+                 (parse (expand-qq exp)))))
+
+
+(define ^quote?
+  (lambda (tag)
+    (lambda (e)
+      (and (pair? e)
+	   (eq? (car e) tag)
+	   (pair? (cdr e))
+	   (null? (cddr e))))))
+
+(define quote? (^quote? 'quote))
+(define unquote? (^quote? 'unquote))
+(define unquote-splicing? (^quote? 'unquote-splicing))
+
+(define const?
+  (let ((simple-sexprs-predicates
+	 (list boolean? char? number? string?)))
+    (lambda (e)
+      (or (ormap (lambda (p?) (p? e))
+		 simple-sexprs-predicates)
+	  (quote? e)))))
+
+(define quotify
+  (lambda (e)
+    (if (or (null? e)
+	    (pair? e)
+	    (symbol? e)
+	    (vector? e))
+	`',e
+	e)))
+
+(define unquotify
+  (lambda (e)
+    (if (quote? e)
+	(cadr e)
+	e)))
+
+(define const-pair?
+  (lambda (e)
+    (and (quote? e)
+	 (pair? (cadr e)))))
+
+(define expand-qq
+  (letrec ((expand-qq
+	    (lambda (e)
+	      (cond ((unquote? e) (cadr e))
+		    ((unquote-splicing? e)
+		     (error 'expand-qq
+		       "unquote-splicing here makes no sense!"))
+		    ((pair? e)
+		     (let ((a (car e))
+			   (b (cdr e)))
+		       (cond ((unquote-splicing? a)
+			      `(append ,(cadr a) ,(expand-qq b)))
+			     ((unquote-splicing? b)
+			      `(cons ,(expand-qq a) ,(cadr b)))
+			     (else `(cons ,(expand-qq a) ,(expand-qq b))))))
+		    ((vector? e) `(list->vector ,(expand-qq (vector->list e))))
+		    ((or (null? e) (symbol? e)) `',e)
+		    (else e))))
+	   (optimize-qq-expansion (lambda (e) (optimizer e (lambda () e))))
+	   (optimizer
+	    (compose-patterns
+	     (pattern-rule
+	      `(append ,(? 'e) '())
+	      (lambda (e) (optimize-qq-expansion e)))
+	     (pattern-rule
+	      `(append ,(? 'c1 const-pair?) (cons ,(? 'c2 const?) ,(? 'e)))
+	      (lambda (c1 c2 e)
+		(let ((c (quotify `(,@(unquotify c1) ,(unquotify c2))))
+		      (e (optimize-qq-expansion e)))
+		  (optimize-qq-expansion `(append ,c ,e)))))
+	     (pattern-rule
+	      `(append ,(? 'c1 const-pair?) ,(? 'c2 const-pair?))
+	      (lambda (c1 c2)
+		(let ((c (quotify (append (unquotify c1) (unquotify c2)))))
+		  c)))
+	     (pattern-rule
+	      `(append ,(? 'e1) ,(? 'e2))
+	      (lambda (e1 e2)
+		(let ((e1 (optimize-qq-expansion e1))
+		      (e2 (optimize-qq-expansion e2)))
+		  `(append ,e1 ,e2))))
+	     (pattern-rule
+	      `(cons ,(? 'c1 const?) (cons ,(? 'c2 const?) ,(? 'e)))
+	      (lambda (c1 c2 e)
+		(let ((c (quotify (list (unquotify c1) (unquotify c2))))
+		      (e (optimize-qq-expansion e)))
+		  (optimize-qq-expansion `(append ,c ,e)))))
+	     (pattern-rule
+	      `(cons ,(? 'e1) ,(? 'e2))
+	      (lambda (e1 e2)
+		(let ((e1 (optimize-qq-expansion e1))
+		      (e2 (optimize-qq-expansion e2)))
+		  (if (and (const? e1) (const? e2))
+		      (quotify (cons (unquotify e1) (unquotify e2)))
+		      `(cons ,e1 ,e2))))))))
+    (lambda (e)
+      (optimize-qq-expansion
+       (expand-qq e)))))
+
+
 
 (define parse
  (let ((run
   (compose-patterns
      _const
      _var
+     _cond
      _if
-     _disjunction
+     _or
+     _and
      _lambda
      _define
-     _application)))
+     _begin
+     _let
+     _let*
+     _letrec
+     _set!
+     _quasiquote
+     _application
+     )))
   (lambda (sexpr)
-        (run sexpr (lambda () '(this is what happens when the tag parser fails to match the input))))))
+        (run sexpr (lambda () "Exception in parser")))))
